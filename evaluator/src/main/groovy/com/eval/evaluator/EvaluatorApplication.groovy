210e9744ce6b5d1116d4e907f1293af948677f02
MacJaguar <duckJaguar@duck.com>
2025-11-12 23:13:00 -0500

    init

---
+++ evaluator/src/main/groovy/com/eval/evaluator/EvaluatorApplication.groovy
@@ -0,0 +1,13 @@
+package com.eval.evaluator
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class EvaluatorApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(EvaluatorApplication, args)
+	}
+
+}
